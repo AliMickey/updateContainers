cc15fb2c3b12f9bcece6fa3d20d6197778408d5e
AliMicky <ad312@pm.me>
2020-08-05 18:22:34 +1000

    v1

--- updateContainers.sh
+++ updateContainers.sh
@@ -6,15 +6,15 @@ username="Docker"
 avatar="https://assets.gitlab-static.net/uploads/-/system/project/avatar/7024001/AppLogo_Docker.png?width=64"
 
 #Add your contiainer names below, place all linuxserver maintained containers first. 
-linuxserver=(bazarr jackett jellyfin letsencrypt lidarr nextcloud ombi plex tautulli)
+lsio="linuxserver/docker"
+linuxserver=($lsio-bazarr $lsio-jackett $lsio-jellyfin $lsio-letsencrypt $lsio-lidarr $lsio-nextcloud $lsio-ombi $lsio-plex $lsio-tautulli)
 other=(authelia/authelia haugene/docker-transmission-openvpn pi-hole/pi-hole traccar/traccar tzahi12345/youtubedl-material)
 
 #Pull images
-#docker-compose pull $linuxserver $other
-
+docker-compose pull $linuxserver $other
 
 #Update loop
-for i in "linuxserver/docker-${linuxserver[@]}" "${other[@]}"
+for i in "${linuxserver[@]}" "${other[@]}"
 do	
 	curl -H "Accept: application/vnd.github.v3.json" https://api.github.com/repos/$i/releases/latest > updateContainersTemp.json
 	version=$(jq -r '.tag_name' updateContainersTemp.json)
@@ -22,9 +22,9 @@ do
 	#If latest version is not recorded in file then update the container.
 	if ! grep "'$i':'$version'" updateContainerVersion.txt
 	then
-		#docker-compose up -d $i
+		docker-compose up -d $i
 		releaseNotes=$(jq '.body' updateContainersTemp.json)
-		url=$(jq '.html_url' updateContainersTemp.json)
+		url=$(jq -r '.html_url' updateContainersTemp.json)
 		curl -H "Content-Type: application/json" \
 			-d '{"username": "'$username'", "avatar_url": "'$avatar'", "embeds": [{"title": "Updated '$i' to '$version'", "url": "'$url'", "description": '"$releaseNotes"', "color": 2332140}]}' \
 			$webhook
